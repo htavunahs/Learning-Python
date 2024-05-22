bbcece31d429f1f457a6111336f62c6948c4a5fd
htavunahs <shanuvathbunjarat@gmail.com>
2024-05-22 17:21:44 +0800

    Final version

--- house.py
+++ house.py
@@ -1,8 +1,9 @@
-name = input("What is your name? ")
+name = input("What is your name? ").capitalize()
 
-if name == "Harry"or"Hermione"or"Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
